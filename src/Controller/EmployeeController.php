c59501622e53f2aaff79a6d37caf82a129f2632a
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-03-21 07:49:39 +0100

    Ensure project link in the navbar remains active on the project and task pages

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EmployeeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EmployeeController extends AbstractController
+{
+    #[Route('/employees', name: 'app_employees')]
+    public function index(EmployeeRepository $employeeRepository): Response
+    {
+        $employees = $employeeRepository->findAll();
+        return $this->render('employee/list.html.twig', [
+            'employees' => $employees,
+        ]);
+    }
+}
